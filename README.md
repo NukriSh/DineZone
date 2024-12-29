ac692e5ac9e3a568d26c6b867db36668764e73d3
Nukri <sh.nukri@gmail.com>
2024-12-29 18:47:17 +0400

    interpolation percantage, propery binding noCurrentOrders

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
- <!-- 1. Components: Utilize multiple Angular components for different sections of the application. 
- 1.2. selector: 'app-search-bar'
+ <!-- Project requirements
+ 1. Components: Utilize multiple Angular components for different sections of the application.
+ 1.2. selector: 'app-search-bar' used in app.component.html
+      selector: 'app-promotions' used in app.component.html
+    interpolation: promotions.component.html uses 'percantage' variable interpolation.
+    property binding: promotions.component.html uses 'noCurrentOrders' boolean.
  -->
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchBarComponent } from "./search-bar/search-bar.component";
+import { PromotionsComponent } from "./promotions/promotions.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchBarComponent],
+  imports: [RouterOutlet, SearchBarComponent, PromotionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'OrderCraftFront';
+  title = 'OrderCraft';
+  orderCount: number = 0;
+
+  noCurrentOrders: boolean =true;
 }
