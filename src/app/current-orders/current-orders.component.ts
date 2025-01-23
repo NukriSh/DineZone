401a3e6e6b26bc10842aba98bfd2dd4293421538
Nukri <sh.nukri@gmail.com>
2025-01-23 22:20:20 +0400

    update current-orders component

--- src/app/current-orders/current-orders.component.ts
+++ src/app/current-orders/current-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ElementRef } from '@angular/core';
 import { ItemCounterComponent } from "../item-counter/item-counter.component";
 
 @Component({
@@ -8,14 +8,23 @@ import { ItemCounterComponent } from "../item-counter/item-counter.component";
   templateUrl: './current-orders.component.html',
   styleUrl: './current-orders.component.scss'
 })
-export class CurrentOrdersComponent {
-  // @Input() currentoOderCount: number = 0;
+export class CurrentOrdersComponent implements AfterContentInit{
+
   currentoOderCount: number = 0;
   noCurrentOrders: boolean = this.currentoOderCount === 0 ? true : false;
 
-
+  constructor() {
+    console.log('CurrentOrdersComponent constructor');
+  }
 
   getItemCount(itemCount: number) {
     this.currentoOderCount = itemCount
+  }
+  // @ts-ignore
+  @ContentChild('projectedContent') contentElement: ElementRef;
+
+  ngAfterContentInit() {
+    console.log('Projected Content:', this.contentElement.nativeElement.textContent)
+    this.contentElement.nativeElement.style.color = 'blue';
   }
 }
