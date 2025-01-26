86e9ddfaa3e7bc1ab286feb30e99be5540ccd277
Nukri <sh.nukri@gmail.com>
2025-01-26 23:18:04 +0400

    add guards to each view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { ContactComponent } from './contact/contact.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { OwnerViewComponent } from './owner-view/owner-view.component';
 import { CustomerViewComponent } from './cutsomer-view/customer-view.component';
-import { AuthGuardService } from './services/auth-guard.service';
+import { AuthGuardService, CanViewAsCustomer, CanViewAsOwner } from './services/auth-guard.service';
 import { GeorgiaComponent } from './contact/georgia/georgia.component';
 import { BulgariaComponent } from './contact/bulgaria/bulgaria.component';
 
@@ -22,12 +22,12 @@ export const routes: Routes = [
     {  
         path: 'customer', 
         component: CustomerViewComponent,
-        canActivate: [AuthGuardService]
+        canActivate: [ AuthGuardService, CanViewAsCustomer ]
     },
     { 
         path: 'owner', 
         component: OwnerViewComponent,
-        canActivate: [AuthGuardService] 
+        canActivate: [AuthGuardService, CanViewAsOwner],
     },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: AppComponent } //this must be always last to not mess up the path matching
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -19,4 +19,56 @@ export class AuthGuardService implements CanActivate {
     }
   }
 
+  // canViewAsOwner (): boolean {
+  //   if ((this.authService.isRestaurantOwnerView()) || (this.authService.isAdminView())) {
+  //     return true;
+  //     } else {
+  //       console.log('auth-guard worked, redirecting to home page');
+  //       this.router.navigate(['/home']);
+  //       return false;
+  //     }
+  // }
+
+  // isCustomer (): boolean {
+  //   if ((this.authService.isCustomerView()) || (this.authService.isAdminView())) {
+  //     return true;
+  //     } else {
+  //       console.log('auth-guard worked, redirecting to home page');
+  //       this.router.navigate(['/home']);
+  //       return false;
+  //     }
+  // }
+
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CanViewAsOwner implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isRestaurantOwnerView() || this.authService.isAdminView()) {
+      return true;
+    }
+    console.log('canViewAsOwner worked, redirecting to home page');
+    this.router.navigate(['/home']);
+    return false;
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CanViewAsCustomer implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isCustomerView() || this.authService.isAdminView()) {
+      return true;
+    }
+    console.log('canViewAsOwner worked, redirecting to home page');
+    this.router.navigate(['/home']);
+    return false;
+  }
 }
