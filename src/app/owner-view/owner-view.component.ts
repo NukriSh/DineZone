c1a80e09d39ca8b0d17596db5e5f3a3231744468
Nukri <sh.nukri@gmail.com>
2025-01-26 15:22:31 +0400

    dropdown for owner

--- src/app/owner-view/owner-view.component.ts
+++ src/app/owner-view/owner-view.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Restaurants } from '../interfaces/restaurants';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-owner-view',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './owner-view.component.html',
   styleUrl: './owner-view.component.scss'
 })
 export class OwnerViewComponent {
 
+    constructor(private dataService: DataService) {}
+  
+    restaurants: Restaurants[] = [];
+    errorMessage: string = '';
+    selectedRestaurantId: number | null = null;
+  
+    ngOnInit() {
+  
+      this.dataService.getRestaurantList().subscribe({
+        next: (restaurants) => {
+          console.log(restaurants);
+          this.restaurants = restaurants;
+        },
+        error: (error) => {
+          this.errorMessage = error;
+        }
+      });
+
+
+    }   
 }
