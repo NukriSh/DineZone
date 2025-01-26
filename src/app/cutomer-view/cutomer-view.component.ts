0393e9d15b48d4f8d784d49531436a5dffbb0563
Nukri <sh.nukri@gmail.com>
2025-01-26 14:20:28 +0400

    customer view encapsulation

--- src/app/cutomer-view/cutomer-view.component.ts
+++ src/app/cutomer-view/cutomer-view.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { ListRestaurantsComponent } from "../list-restaurants/list-restaurants.component";
+import { DataService } from '../services/data.service';
+import { Restaurants } from '../interfaces/restaurants';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cutomer-view',
   standalone: true,
-  imports: [ListRestaurantsComponent],
+  imports: [ NgFor],
   templateUrl: './cutomer-view.component.html',
   styleUrl: './cutomer-view.component.scss'
 })
 export class CutomerViewComponent {
+
+  constructor(private dataService: DataService) {}
+
+  restaurants: Restaurants[] = [];
+  errorMessage: string = '';
+
+  ngOnInit() {
+
+    this.dataService.getRestaurantList().subscribe({
+      next: (restaurants) => {
+        console.log(restaurants);
+        this.restaurants = restaurants;
+      },
+      error: (error) => {
+        this.errorMessage = error;
+      }
+    });
+
+  }
 
 }
